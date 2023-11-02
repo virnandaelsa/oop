ea412b2612d3e7e2a90b926684bb1d79d8541be7
Amdzak <ahmadzulfikar133@gmail.com>
2023-11-02 20:16:17 +0700

    add file

--- Pemilik/Pemilik.py
+++ Pemilik/Pemilik.py
@@ -46,7 +46,7 @@ class Pemilik:
         for key in myresult:
             print(f"NIK :{key[1]} |",end="")
             print(f"Nama :{key[2]} |",end="")
-            print(f"Alamat :{key[2]} |",end="")
+            print(f"Alamat :{key[3]} |",end="")
             print(f"Jenis Kelamin :{key[4]} |",end="")
             print(f"No hp :{key[5]} |",end="")
             print(f"Status :{key[6]} |")
